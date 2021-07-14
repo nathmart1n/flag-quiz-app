a11a3994bd887b32c4395fd23d7fdef57e55c15e
nathmart <nathmart@umich.edu>
2021-07-14 16:32:31 -0700

    Adding country images

--- src/App.js
+++ src/App.js
@@ -1,64 +1,58 @@
 import React, { useState } from 'react';
 
 export default function App() {
+	//Country codes and names object
+	var countries = JSON.parse(process.env.PUBLIC_URL + 'alpha2.json');
+	//Initialize our first question number and track question number
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	//Initialize score and track
+	const [score, setScore] = useState(0);
+	//Function to update current question number
+	const handleAnswerButtonClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	//Handle reaching end and display score screen
+	const [showScore, setShowScore] = useState(false);
+	//Questions we use
 	const questions = [
 		{
-			questionText: 'What is the capital of France?',
+			questionImagePath: process.env.PUBLIC_URL + 'images/1.png',
 			answerOptions: [
 				{ answerText: 'New York', isCorrect: false },
 				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Andorra', isCorrect: true },
 				{ answerText: 'Dublin', isCorrect: false },
 			],
 		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
+		
 	];
 
 	return (
 		<div className='app'>
 			{/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
-			{false ? (
-				<div className='score-section'>You scored 1 out of {questions.length}</div>
+			{showScore ? (
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question 1</span>/{questions.length}
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
 						</div>
-						<div className='question-text'>{questions[0]}</div>
+						<img src={questions[currentQuestion].questionImagePath} alt='Country'/>
 					</div>
 					<div className='answer-section'>
-						<button>Answer 1</button>
-						<button>Answer 2</button>
-						<button>Answer 3</button>
-						<button>Answer 4</button>
+					{questions[currentQuestion].answerOptions.map((answerOption, index) => (
+						<button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+					))}
 					</div>
 				</>
 			)}
