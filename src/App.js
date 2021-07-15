9f58f819b566512726f90d638787145a7cc95aea
nathmart <nathmart@umich.edu>
2021-07-14 17:46:56 -0700

    Now works and pulls from country database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
+import countries from './data/alpha2.json';
 
 export default function App() {
-	//Country codes and names object
-	var countries = JSON.parse(process.env.PUBLIC_URL + 'alpha2.json');
 	//Initialize our first question number and track question number
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	//Initialize score and track
@@ -13,44 +12,74 @@ export default function App() {
 			setScore(score + 1);
 		}
 		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
+		if (nextQuestion < shuffledSlicedQuestions.length) {
 			setCurrentQuestion(nextQuestion);
 		} else {
 			setShowScore(true);
 		}
 	};
+	//Array shuffle func
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
 	//Handle reaching end and display score screen
 	const [showScore, setShowScore] = useState(false);
 	//Questions we use
-	const questions = [
+	/*
 		{
-			questionImagePath: process.env.PUBLIC_URL + 'images/1.png',
+			questionImagePath: 'images/am.png',
 			answerOptions: [
 				{ answerText: 'New York', isCorrect: false },
 				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Andorra', isCorrect: true },
+				{ answerText: 'Paris', isCorrect: true },
 				{ answerText: 'Dublin', isCorrect: false },
 			],
 		},
-		
-	];
+
+	*/
+	
+	const questions = [];
+	const countryCodes = Object.keys(countries);
+
+	for (var key in countries) {
+		var question = {
+				questionImagePath: 'images/' + key.toLowerCase() + '.png',
+				answerOptions: [
+					{ answerText: countries[countryCodes[Math.floor(Math.random() * countryCodes.length)]], isCorrect: false },
+					{ answerText: countries[countryCodes[Math.floor(Math.random() * countryCodes.length)]], isCorrect: false },
+					{ answerText: countries[key], isCorrect: true },
+					{ answerText: countries[countryCodes[Math.floor(Math.random() * countryCodes.length)]], isCorrect: false },
+				],
+			}
+		shuffleArray(question.answerOptions)
+		questions.push(question)
+	}
+
+	shuffleArray(questions);
+
+	const shuffledSlicedQuestions = questions.slice(0, 10);
 
 	return (
 		<div className='app'>
 			{/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
 			{showScore ? (
-				<div className='score-section'>You scored {score} out of {questions.length}</div>
+				<div className='score-section'>You scored {score} out of {shuffledSlicedQuestions.length}</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+							<span>Question {currentQuestion + 1}</span>/{shuffledSlicedQuestions.length}
 						</div>
-						<img src={questions[currentQuestion].questionImagePath} alt='Country'/>
+						<img src={shuffledSlicedQuestions[currentQuestion].questionImagePath} alt='Country'/>
 					</div>
 					<div className='answer-section'>
-					{questions[currentQuestion].answerOptions.map((answerOption, index) => (
+					{shuffledSlicedQuestions[currentQuestion].answerOptions.map((answerOption, index) => (
 						<button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
 					))}
 					</div>
